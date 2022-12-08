55eb9560df5c54dd17d9b97cf2921eac98efc69c
Ericson Nascimento <ericson@grupocyber.com.br>
2022-12-08 16:45:31 -0300

    operadores

---
+++ Modulo1/1-exemplo_sintax.php
@@ -0,0 +1,7 @@
+<?php
+$_nomeCurso = "Programação de página dinâmica PHP";
+$_anoCriacao = 1999;
+
+echo $_nomeCurso . ", Criado em: " . $_anoCriacao;
+die;
+
---
+++ Modulo1/3-operadores_aritimeticos.php
@@ -0,0 +1,18 @@
+<?php
+
+$var1 = 11;
+$var2 = 2;
+
+$modulo = $var1 % $var2;
+
+echo "Resultado do módulo (resto da divisão % entre 11 e 2): "  . $modulo . "\n";
+
+$var3 = 10;
+$var4 = 2;
+
+$exponencial = $var3 ** $var4;
+
+echo nl2br("\n\n Resultado do exponencial (potência ** entre 10 e 2): "  . $exponencial);
+
+//nl2br() faz a quebra de linha no comando echo \n\n quebra duas linha e \n quebra uma linha
+
